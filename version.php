4a623b4a8857932807206df646720c22c9367434
Juan Pablo <juan.pablo.de.castro@gmail.com>
2021-09-26 06:50:14 +0000

    Version bump

--- version.php
+++ version.php
@@ -27,3 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2021092500;              // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2020110300;              // Requires this Moodle version.
 $plugin->component = 'format_redirected'; // Full name of the plugin (used for diagnostics).
+$plugin->release = '0.0.2';
+$plugin->maturity = MATURITY_STABLE;

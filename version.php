a014a1815e53358008acc7a0c302539588449367
Juan Pablo <juan.pablo.de.castro@gmail.com>
2021-09-26 16:03:06 +0000

    Bump versions

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021092500;              // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2021092600;              // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2018120300;              // Requires this Moodle version.
 $plugin->component = 'format_redirected'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '0.0.2';
+$plugin->release = '0.0.3';
 $plugin->maturity = MATURITY_STABLE;
